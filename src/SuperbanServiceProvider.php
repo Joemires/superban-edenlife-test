e1b97c241d5e13b2013f8f5173afc60c77b4ea97
Joseph Bassey <Joemires20@yahoo.com>
2023-12-19 13:49:28 +0100

    Setup superban package

---
+++ src/SuperbanServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Joemires\Superban;
+
+use Illuminate\Support\ServiceProvider;
+
+final class SuperbanServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
